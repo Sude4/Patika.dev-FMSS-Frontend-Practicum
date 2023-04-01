6d70ebe034e30dfcfaf4e9352bfa5b6dbd855bed
Meryem Sude ÇETİN <56775168+Sude4@users.noreply.github.com>
2023-04-01 15:44:14 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const getUser =async(id_) => {
+    const {data : user} = await axios("https://jsonplaceholder.typicode.com/users/"+ id_);
+    console.log(user);
+};
+
+const getPost = async(id_) => {
+    const {data : post} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${id_}`);
+    console.log(post); 
+};
+
+function getData(id_) {
+    try {
+    const user = getUser(id_);
+    const post = getPost(id_);
+    }
+    catch (error) {
+        return(error);
+    }
+}
+
+export default getData;
